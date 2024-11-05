b8dac988f13a3fcd26fb9f60c18999f6c516b1a9
Bosahan R.A <132349894+bosahan@users.noreply.github.com>
2024-11-05 10:02:52 +0530

    Add files via upload

---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+    // Temp fonts
+    fonts: {
+      title: "Space Grotesk, sans-serif",
+      main: "Space Grotesk, sans-serif"
+    },
+    // Colors for layout
+    colors: {
+      primary1: "#854CE6",
+      background1: "#F9FBE7",
+      button: "#854CE6",
+      background2: "#F9FBE7",
+      text: "#FFE300",
+      text1: "#FFE300",
+      text2: "#FFE300",
+      text3: "#FFE300",
+      footerBackground: "#F9FBE7"
+    },
+    // Breakpoints for responsive design
+    breakpoints: {
+      sm: 'screen and (max-width: 640px)',
+      md: 'screen and (max-width: 768px)',
+      lg: 'screen and (max-width: 1024px)',
+      xl: 'screen and (max-width: 1280px)'
+    },
+  }
